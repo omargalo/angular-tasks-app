cb7496c7de21509b011c73ebbcbcf7d9b69c649f
Omar Garcia <omar.garcia@omargl.net>
2025-01-12 14:56:09 -0600

    ready, set, go

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Evelyn Garcia',
+      avatar: 'user-1.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'Omar Garcia',
+      avatar: 'user-2.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Marcela Aragon',
+      avatar: 'user-3.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'Alexander Garcia',
+      avatar: 'user-4.jpg',
+    },
+    {
+      id: 'u5',
+      name: 'Ender Garcia',
+      avatar: 'user-5.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'Victoria Lopez',
+      avatar: 'user-6.jpg',
+    },
+  ];
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+//helper to select random dummy user
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log('Clicked on user: ', this.selectedUser);
+  }
+
+}
