ee3c815a246d11709acd2c9b6c48c1322f00f203
Omar Garcia <omar.garcia@omargl.net>
2025-01-13 11:26:37 -0600

    using signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 //helper to select random dummy user
@@ -12,14 +12,23 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  //selectedUser = DUMMY_USERS[randomIndex];
 
-  get imagePath() {
+  // Using signal to notify the parent component
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  /* get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar;
-  }
+  } */
+
+  // Read the value of the signal using computed property
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    //this.selectedUser = DUMMY_USERS[randomIndex];
+
+    // Change the value of the signal
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
